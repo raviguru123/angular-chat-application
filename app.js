e99fd9e541ae99bd72df565866696b31d3a4a8b2
raviguru123 <raviguruiitr@gmail.com>
2016-05-27 00:56:15 +0530

    chat application first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var app=require("express")();
+var http=require('http').Server(app);
+var io=require("socket.io")(http);
+var _ = require('underscore');
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.get("/",function(req,res){
+
+	res.sendFile(__dirname+"/index.html");
+});
+
+
+
+
+io.on('connection', function(socket){
+	socket.on("connect", function () {  
+		console.log("Connected!");
+	});
+	socket.on('chat message', function(msg){
+		console.log("message=:",msg);
+		io.emit("chat message",msg);
+		socket.broadcast.emit('chat message', msg);
+	});
+});
+http.listen(3000,function(){
+	console.log("server listen on port 3000");
+});
---
+++ js/index.js
@@ -0,0 +1,5 @@
+var app=angular.module("chat.module",['ngMaterial']);
+
+app.controller('ChatController', ['$scope', function($scope){
+	
+}])
