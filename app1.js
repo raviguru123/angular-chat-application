6464ced09585c038538454f1039291531fac76b3
raviguru123 <raviguruiitr@gmail.com>
2016-06-08 10:01:15 +0530

    add advance chat messagubg system in app1.js and index.html

--- app1.js
+++ app1.js
@@ -7,14 +7,37 @@ app.use(express.static(__dirname+"/"));
 http.listen(3000,function(){
 	console.log("server is running=",__dirname);
 });
-
 app.get("/",function(req,res){
 	res.sendFile(__dirname+"/index.html");
 });
-
-io.on("connection",function(socket){
+var users=[];
+io.sockets.on("connection",function(socket){
 	socket.on("send message",function(msg){
 		console.log("message=",msg);
-		io.emit("new message",msg);
+		io.sockets.emit("new message",msg);
+	});
+	socket.on("set user",function(data,callback){
+		console.log("user=",data);
+		if(users.indexOf(data)!=-1)
+		{
+			callback(false);
+		}
+		else
+		{
+			callback(true);
+			socket.user=data;
+			users.push(socket.user);
+			updateuser();
+			
+		}
 	})
+	function updateuser(){
+		io.sockets.emit("usernames",users);
+	}
+	socket.on("disconnect",function(){
+		//console.log('disconnect',socket.user);
+		if(!socket.user)return;
+		users.splice(users.indexOf(socket.user),1);
+		updateuser();
+	});
 })
